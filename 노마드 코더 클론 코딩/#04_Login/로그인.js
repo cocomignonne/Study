cd6e62ae2750765f53ed566b2fb7710f8550c05c
cocomignn <skekkjim@naver.com>
2021-12-10 13:46:04 +0900

    12/10일 인강정리

--- 노마드 코더 클론 코딩/#04_Login/로그인.js
+++ 노마드 코더 클론 코딩/#04_Login/로그인.js
@@ -6,18 +6,17 @@ const loginInput = document.querySelector('#login-form input');
 const loginButton = document.querySelector('#login-form button');
 
 function onLoginBtnClick() {
-    const userName = loginInput.value;
-    console.log(userName); 
+    let userName = loginInput.value;
     // if(userName === '') {
-    //     alert('Please write ur name!')
+    //     alert('Please write ur name!') 
     // } else if(userName.length > 15 ) {
     //     alert('Ur name is too long!')
     // }
+    console.log(userName);  
+    //form태그 안에 있는 button이나 type이 submit인 input을 클릭하면 내가작성한 데이터가 넘어가기 때문에 콘솔에 찍히지 않음!!!!!!!
 };
 
-
-
-loginInput.addEventListener('click', onLoginBtnClick);
+loginButton.addEventListener('click', onLoginBtnClick);
 
 
 
